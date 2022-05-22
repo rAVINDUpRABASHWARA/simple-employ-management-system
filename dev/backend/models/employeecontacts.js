cf4517cac604936fb0a3ddb7983f886edcbf66de
rAVINDUpRABASHWARA <00prabashwara123@gmail.com>
2022-05-22 21:36:00 +0530

    Create employee add backend route

--- dev/backend/models/employeecontacts.js
+++ dev/backend/models/employeecontacts.js
@@ -1,5 +1,7 @@
+//To connect the database use the mongoose and following lin use to import the mongoose
 import mongoose from "mongoose";
 
+//In mongodb there is something call schema, so there is a method in mongoose and assign that method to a constant variable
 const employeeContactSchema = new mongoose.Schema (
     {
         EmpId: {
@@ -13,5 +15,6 @@ const employeeContactSchema = new mongoose.Schema (
     }
 );
 
+//data is add to the routes and that data will send to the DB through modles.
 const empContact = mongoose.model('employee_contact', employeeContactSchema);
 export default empContact;
