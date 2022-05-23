3635c6517167350480e6a8628403f319df83173b
rAVINDUpRABASHWARA <00prabashwara123@gmail.com>
2022-05-23 18:22:42 +0530

    connect login function with frontend

--- dev/frontend/src/components/Login.jsx
+++ dev/frontend/src/components/Login.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react'
 
 export default function Login() {
+
+    const [employee, setEmployee] = useState({
+        NIC : '',
+        password : ''
+    });
+
+    //Handle Input
+    const handleChange = (event) => {
+        let name = event.target.name
+        let value = event.target.value
+
+        setEmployee({...employee, [name]:value})
+    }
+
+    //Handle Login
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const {NIC, password} = employee;
+        try {
+            const res = await fetch('/login', {
+                method : "POST",
+                headers : {
+                    "Content-Type" : "application/json"
+                },
+                body : JSON.stringify({
+                    NIC, password
+                })
+            });
+
+            if(res.status === 400 || !res) {
+                window.alert("Invalid Cradentials");
+            } else {
+                window.alert("Login Successfull");
+                window.location.reload();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div>
             <div className="container shadow my-5">
@@ -12,19 +51,19 @@ export default function Login() {
                     </div>
                     <div className="col-md-6 p-5">
                         <h1 className="display-6 fw-bolder mb-5">LOGIN</h1>
-                            <form>
+                            <form onSubmit={handleSubmit}>
                                 <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                                    <label for="exampleInputEmail1" class="form-label">NIC Number</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='NIC' value={employee.NIC} onChange={handleChange} />
+                                    <div id="emailHelp" class="form-text">We'll never share your login details with anyone else.</div>
                                 </div>
                                 <div class="mb-3">
                                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1"/>
+                                    <input type="password" class="form-control" id="exampleInputPassword1" name='password' value={employee.password} onChange={handleChange} />
                                 </div>
                                 <div class="mb-3 form-check">
                                     <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    <label class="form-check-label" for="exampleCheck1">Remember Me</label>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Submit</button>
                             </form>
