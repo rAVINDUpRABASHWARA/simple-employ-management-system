c372792afe1bbf13f9f092644eb2bb1a45ca5e36
rAVINDUpRABASHWARA <00prabashwara123@gmail.com>
2022-05-23 12:38:35 +0530

    test some functions

--- dev/frontend/src/App.js
+++ dev/frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from 'react';
+import AddEmployeeScreen from './Screens/AddEmployeeScreen';
 
 function App() {
   return (
     <div className="App">
       <h1>Employee Management System</h1>
+      <main>
+        <AddEmployeeScreen/>
+      </main>
     </div>
   );
 }
